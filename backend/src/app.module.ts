503bda38c42bc875061fff368a8fc4b90f0d3774
karinamaulitova <karinamaulitova@gmail.com>
2021-10-09 21:54:23 +0300

    move db credentials to env config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,15 +6,15 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MessagesModule,
     ConfigModule.forRoot(),
+    MessagesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'pass123',
+      database: process.env.DB_NAME || 'postgres',
       autoLoadEntities: true,
       synchronize: process.env.NODE_ENV !== "production"
     }),
